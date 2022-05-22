61e6dc996025a61acfd6324edf5008706a35195a
dom thomas <domtom1126@gmail.com>
2022-05-22 19:44:07 -0400

    Some light work dog

--- get_news.py
+++ get_news.py
@@ -7,5 +7,10 @@ def getNews():
 
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all('a', class_='titlelink', href=True)
+    # returns a list of all the links on the page on serpate lines 
+    for link in results:
+        link_list = [link.get('href')]
+        return link_list[:10]
+    
 
-    return results
+getNews()
