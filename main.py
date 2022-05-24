3d45880d7ed762be5b370333926880072eef13e1
dom thomas <domtom1126@gmail.com>
2022-05-23 21:32:17 -0400

    Cleaned up

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import smtplib
 import os
 
+from get_news import getNews
+
+# Email address and password come from env variables
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
     smtp.starttls()
-    smtp.ehlo()
 
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
     subject = 'Test email'
-    body = 'This is a test email from the python script.'
+    body = getNews()
 
     msg = f'Subject: {subject}\n\n{body}'
 
